942cedea60b3dc0aad21f0b1d89285dc89addc5a
echen1227 <dinosaurkiller_x@yahoo.com.tw>
2015-08-06 18:46:16 +0800

    gl

--- coursera/assignment/assignment2/assignment2.js
+++ coursera/assignment/assignment2/assignment2.js
@@ -3,12 +3,20 @@
 var canvas;
 var canvasRect
 var gl;
-var maxNumVertices  = 512*512;
+var maxNumVertices;
+
 var index = 0;
+var vertexIndex = 0;
+var xIndex;
+var yIndex;
+
 var inputColor;
 var startIndex = [];
 var mouseDown = false;
 var clearButton;
+
+var vertexArray = [];
+var colorArray = [];
 
 
 window.onload = function init() {
@@ -16,12 +24,18 @@ window.onload = function init() {
     canvasRect = canvas.getBoundingClientRect();
     inputColor = document.getElementById("inputColor");
     clearButton = document.getElementById("clear-button");
+    maxNumVertices = canvas.width*canvas.height;
+
+// maxNumVertices= 8;
 
     gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     canvas.addEventListener("mousedown", function(event){
         mouseDown = true;
+        xIndex = event.clientX - canvasRect.left;
+        yIndex = event.clientY - canvasRect.top;
+        index = yIndex*canvas.width + xIndex;
         startIndex.push(index);
     });
 
@@ -33,31 +47,28 @@ window.onload = function init() {
 
         if(mouseDown)
         {
+            xIndex = event.clientX - canvasRect.left;
+            yIndex = event.clientY - canvasRect.top;
+            index = yIndex*canvas.width + xIndex;
+
             gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
-            var temp = vec2(2*(event.clientX - canvasRect.left)/canvas.width-1, 2*(canvas.height-(event.clientY - canvasRect.top))/canvas.height-1);
-            gl.bufferSubData(gl.ARRAY_BUFFER, 8*index, flatten(temp));
+            var temp = vec4(2*xIndex/canvas.width-1, 2*(canvas.height-yIndex)/canvas.height-1, 0.0, 1.0);
+            gl.bufferSubData(gl.ARRAY_BUFFER, 16*index, flatten(temp));
 
             gl.bindBuffer(gl.ARRAY_BUFFER, cBuffer);
             temp = vec4(HexToR(inputColor.value),HexToG(inputColor.value),HexToB(inputColor.value),1.0);
             gl.bufferSubData(gl.ARRAY_BUFFER, 16*index, flatten(temp));
-            ++index;
+            ++vertexIndex;
 
             gl.clear( gl.COLOR_BUFFER_BIT );
-            if(startIndex.length > 1)
-            {
-                for(var i =0; i < startIndex.length; i++)
-                {
-                    gl.drawArrays( gl.LINE_STRIP, startIndex[i], startIndex[i+1]-startIndex[i] );
-                }
-            }
-            gl.drawArrays( gl.LINE_STRIP, startIndex[startIndex.length-1], index - startIndex[startIndex.length-1]);
-
-            // if(index >= maxNumVertices)
+            // if(startIndex.length > 1)
             // {
-            //     index = 0;
-            //     startIndex = [];
-            //     mouseDown = false;
+            //     for(var i =0; i < startIndex.length; i++)
+            //     {
+            //         gl.drawArrays( gl.LINE_STRIP, startIndex[i], startIndex[i+1]-startIndex[i] );
+            //     }
             // }
+            gl.drawArrays( gl.LINE_STRIP, 0, maxNumVertices);
         }
     } );
     clearButton.addEventListener("click", function(event){
@@ -77,18 +88,26 @@ window.onload = function init() {
     var program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
+    for(var i = 0; i < canvas.height; i++)
+    {
+        for (var j = 0; j < canvas.width; j++) {
+            vertexArray.push(vec4(i,j,0.0,1.0));
+            colorArray.push(vec4(0.5, 0.5, 0.0, 1.0));
+        };
+    }
+
 
     var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, 8*maxNumVertices, gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(vertexArray), gl.STATIC_DRAW );
 
     var vPosition = gl.getAttribLocation(program, "vPosition");
-    gl.vertexAttribPointer(vPosition, 2, gl.FLOAT, false, 0, 0);
+    gl.vertexAttribPointer(vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray(vPosition);
 
     var cBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
-    gl.bufferData( gl.ARRAY_BUFFER, 16*maxNumVertices, gl.STATIC_DRAW );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colorArray), gl.STATIC_DRAW );
 
     var vColor = gl.getAttribLocation( program, "vColor" );
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
--- coursera/assignment/assignment3/assignment3.js
+++ coursera/assignment/assignment3/assignment3.js
@@ -3,7 +3,7 @@
 var gl;
 var canvas;
 var canvasRect;
-var Axis;
+var program;
 
 var near = -10;
 var far = 10;
@@ -20,32 +20,118 @@ var bottom = -2.0;
 var modelViewMatrix, projectionMatrix;
 var modelViewMatrixLoc, projectionMatrixLoc;
 var eye;
-const at = vec3(0.0, 0.0, 0.0);
+const at = vec3(1.0, 1.0, 1.0);
 const up = vec3(0.0, 1.0, 0.0);
+
+var points;
+var colors;
+var axisVB;
+var axisCB;
 
 $(document).ready(function(){
 
 	$("#gl-canvas").click(function(event){
 	});
+
+    $("#create-button").click(function(event){
+
+        var value = $('input[name="model"]:checked').val();
+        if(value == "cube")
+        {
+            console.log("cube");
+            drawCube();
+        }
+    });
+
 });
 
 window.onload = function init() {
 
 	canvas = document.getElementById( "gl-canvas" );
 	canvasRect = canvas.getBoundingClientRect();
-	drawAxis();
 
 	gl = WebGLUtils.setupWebGL( canvas );
     if ( !gl ) { alert( "WebGL isn't available" ); }
 
     gl.viewport( 0, 0, canvas.width, canvas.height );
     gl.clearColor( 0.5, 0.5, 0.5, 1.0 );
-    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    program = initShaders( gl, "vertex-shader", "fragment-shader" );
     gl.useProgram( program );
 
-    var vBuffer = gl.createBuffer();
+    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
+    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
+
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    eye = vec3( radius*Math.sin(theta)*Math.cos(phi),
+                radius*Math.sin(theta)*Math.sin(phi),
+                radius*Math.cos(theta));
+
+    modelViewMatrix = lookAt(eye, at , up);
+    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
+
+    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
+    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
+
+    drawAxis();
+
+}
+
+function drawAxis()
+{
+	points = [ vec4(100.0, 0.0, 0.0, 1),
+			     vec4(-100.0, 0.0, 0.0, 1),
+			     vec4(0.0, 100.0, 0.0, 1),
+			     vec4(0.0, -100.0, 0.0, 1),
+			     vec4(0.0, 0.0, 100.0, 1),
+			     vec4(0.0, 0.0, -100.0, 1)];
+
+    colors = [  vec4(1.0,0.0,0.0,1.0),
+                vec4(1.0,0.0,0.0,1.0),
+                vec4(0.0,1.0,0.0,1.0),
+                vec4(0.0,1.0,0.0,1.0),
+                vec4(0.0,0.0,1.0,1.0),
+                vec4(0.0,0.0,1.0,1.0)];
+
+    axisVB = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, axisVB);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
+    var vPosition = gl.getAttribLocation( program, "vPosition");
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
+    gl.enableVertexAttribArray( vPosition);
+
+    axisCB = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, axisCB );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    gl.drawArrays( gl.LINES, 0, 6 );
+}
+
+function drawCube()
+{
+
+    points = [];
+    colors = [];
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+
+
+
+gl.bindBuffer( gl.ARRAY_BUFFER, axisVB);
+gl.bindBuffer( gl.ARRAY_BUFFER, axisCB );
+gl.drawArrays( gl.LINES, 0, 6 );
+
+var vBuffer = gl.createBuffer();
     gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer);
-    gl.bufferData( gl.ARRAY_BUFFER, flatten(Axis), gl.STATIC_DRAW);
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW);
     var vPosition = gl.getAttribLocation( program, "vPosition");
     gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0);
     gl.enableVertexAttribArray( vPosition);
@@ -57,32 +143,34 @@ window.onload = function init() {
     gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
     gl.enableVertexAttribArray( vColor );
 
-    modelViewMatrixLoc = gl.getUniformLocation( program, "modelViewMatrix" );
-    projectionMatrixLoc = gl.getUniformLocation( program, "projectionMatrix" );
-
-    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
-
-    eye = vec3(radius*Math.sin(theta)*Math.cos(phi),
-        radius*Math.sin(theta)*Math.sin(phi), radius*Math.cos(theta));
-
-    modelViewMatrix = lookAt(eye, at , up);
-    projectionMatrix = ortho(left, right, bottom, ytop, near, far);
-
-    gl.uniformMatrix4fv( modelViewMatrixLoc, false, flatten(modelViewMatrix) );
-    gl.uniformMatrix4fv( projectionMatrixLoc, false, flatten(projectionMatrix) );
-
-    gl.drawArrays( gl.LINES, 0, 2 );
-    gl.drawArrays( gl.LINES, 2, 2 );
-    gl.drawArrays( gl.LINES, 4, 2 );
-
+    console.log(points.length);
+    for( var i=0; i<points.length; i+=3)
+    {
+        gl.drawArrays( gl.LINE_LOOP, i, 3 );
+    }
 }
-
-function drawAxis()
+function quad(a, b, c, d)
 {
-	Axis = [vec4(100.0, 0.0, 0.0, 1),
-			vec4(-100.0, 0.0, 0.0, 1),
-			vec4(0.0, 100.0, 0.0, 1),
-			vec4(0.0, -100.0, 0.0, 1),
-			vec4(0.0, 0.0, 100.0, 1),
-			vec4(0.0, 0.0, -100.0, 1)];
+    var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5, -0.5, -0.5, 1.0 )
+    ];
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        colors.push(vec4(1.0, 0.0, 0.0, 1.0 ));
+    }
+
 }
